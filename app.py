758a49eb65c3686a87e4482865abf08f64e02475
Arun Babu Neelicattu <abn@redhat.com>
2013-07-31 09:36:18 +1000

    When deployed check for $OPENSHIFT_DATA_DIR/victimsweb.cfg

--- app.py
+++ app.py
@@ -33,8 +33,12 @@ DB_NAME = os.environ['OPENSHIFT_APP_NAME']
 if len(DB_NAME.strip()) == 0:
     DB_NAME = 'victimsweb'
 os.environ['VICTIMS_DB_NAME'] = DB_NAME
-os.environ['VICTIMS_CONFIG'] = os.path.join(
-    os.environ['OPENSHIFT_REPO_DIR'], 'config', 'victimsweb.cfg')
+config_file = os.path.join(
+    os.environ['OPENSHIFT_DATA_DIR'], 'victimsweb.cfg')
+if not os.path.exists(config_file):
+    config_file = os.path.join(
+        os.environ['OPENSHIFT_REPO_DIR'], 'config', 'victimsweb.cfg')
+os.environ['VICTIMS_CONFIG'] = config_file
 
 # Start the application
 from victims_web.application import app as application
