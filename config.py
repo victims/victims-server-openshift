6236f133dda57338405beb458c05843f70521524
Jason Shepherd <jshepherd@redhat.com>
2017-08-28 16:56:47 +1000

    running on managed platform

---
+++ config.py
@@ -0,0 +1,44 @@
+from os import environ, makedirs
+from os.path import isfile, isdir, join
+from datetime import timedelta
+from imp import load_source
+from logging import getLogger, INFO as LOG_LEVEL_INFO
+
+# Base Dir
+VICTIMS_BASE_DIR = '/opt/app-root/'
+
+LOGGER = getLogger()
+LOG_FOLDER = VICTIMS_BASE_DIR, join(VICTIMS_BASE_DIR, 'logs')
+LOG_LEVEL = LOG_LEVEL_INFO
+
+FLASK_HOST = environ.get('FLASK_HOST', '0.0.0.0')
+FLASK_PORT = int(environ.get('FLASK_PORT', 8080))
+
+DEBUG = False
+TESTING = False
+
+# File upload
+UPLOAD_FOLDER = join(VICTIMS_BASE_DIR, "uploads")
+ALLOWED_EXTENSIONS = set(['egg', 'jar', 'gem'])
+
+# File download
+DOWNLOAD_FOLDER = join(VICTIMS_BASE_DIR, "downloads")
+
+CACHE_DIR = join(VICTIMS_BASE_DIR, 'cache')
+
+# MongoDB Configuration
+MONGODB_SETTINGS = {
+    'DB': environ.get('VICTIMS_DB', 'victims'),
+    'HOST': environ.get('MONGODB_HOST', 'mongodb.victims.svc'),
+    'PORT': 27017,
+    'USERNAME': environ.get('MONGODB_USERNAME', ''),
+    'PASSWORD': environ.get('MONGODB_PASSWORD', '')
+}
+
+# Hashing commands for each group
+# This will be used as command.format(archive=filename)
+# Eg: 'java': 'victims-java hash {archive!s}'
+HASHING_COMMANDS = {
+    'java': VICTIMS_BASE_DIR + 'src/bin/victims-hash-java {archive!s}',
+}
+
